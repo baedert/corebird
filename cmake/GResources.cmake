9033f0193ba27531e9cc2de4d0f8956cf5eccd52
Timm Bäder <baedert@gmail.com>
2013-08-21 00:12:41 +0200

    Revert "Implement GResources cmake macro"
    This reverts commit 1e8052ad35691688085469442f19e0436dbd0ca8.

--- cmake/GResources.cmake
+++
@@ -1,5 +0,0 @@
-
-
-macro (compile_resources RESOURCES_FILE OUTPUT_FILE)
-  execute_process (COMMAND glib-compile-resources ${RESOURCES_FILE} --target=${OUTPUT_FILE} --generate-source)
-endmacro (compile_resources)
