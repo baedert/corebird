c16a849d96b48e81c7baadf24e5d8881a1a0d469
Timm Bäder <baedert@gmail.com>
2013-09-09 07:00:08 +0200

    add cmake/RenderIcons.cmake to render the icons

---
+++ cmake/GResources.cmake
@@ -0,0 +1,36 @@
+#  This file is part of corebird, a Gtk+ linux Twitter client.
+#  Copyright (C) 2013 Timm Bäder
+#
+#  corebird is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  corebird is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with corebird.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Old command:
+# glib-compile-resources resources.xml --target=src/resources.c --generate-source
+
+
+
+
+macro (add_resources RESOURCE_FILE DST)
+  message ("Add GResource ${RESOURCE_FILE}")
+
+  add_custom_command (OUTPUT ${DST}
+                      COMMAND "glib-compile-resources"
+                      ARGS
+                        "${RESOURCE_FILE}"
+                        "--target=${DST}"
+                        "--generate-source"
+                        DEPENDS ${RESOURCE_FILE})
+  add_custom_target (gresources DEPENDS ${RESOURCE_FILE})
+
+endmacro (add_resources)
