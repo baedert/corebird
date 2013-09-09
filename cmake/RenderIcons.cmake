d768671194e786627d0d7776f384b2946d9e1479
Timm Bäder <baedert@gmail.com>
2013-09-09 06:55:10 +0200

    Automatically build icons

--- cmake/RenderIcons.cmake
+++ cmake/RenderIcons.cmake
@@ -5,20 +5,24 @@ include (ParseArguments)
 macro (render_icon ICON_NAME)
   parse_arguments(ARGS "SIZES" "" ${ARGN})
   message (STATUS "Icons will be installed into ${ICON_DEST_DIR} from ${ICON_SOURCE_DIR}")
+  set (SRC "${CMAKE_CURRENT_SOURCE_DIR}/${ICON_SOURCE_DIR}")
+  set (DST "${CMAKE_CURRENT_SOURCE_DIR}/${ICON_DEST_DIR}")
+
+
+  # Our 'icons' target
+  add_custom_target (icons ALL COMMENT "build icons")
 
   set (output_files "")
   foreach (size ${ARGS_SIZES})
     list (APPEND output_files "${size}x${size}/cb.png")
+    execute_process (COMMAND ${CMAKE_COMMAND} -E make_directory "${DST}/${size}x${size}")
   endforeach (size ${ARGS_SIZES})
 
 
 
-  add_custom_target (icons COMMENT "build icons")
 
   message (STATUS "${ICON_NAME} will be rendered to ${output_files}")
 
-  set (SRC "${CMAKE_CURRENT_SOURCE_DIR}/${ICON_SOURCE_DIR}")
-  set (DST "${CMAKE_CURRENT_SOURCE_DIR}/${ICON_DEST_DIR}")
 
   foreach (size ${ARGS_SIZES})
     add_custom_command (TARGET icons
