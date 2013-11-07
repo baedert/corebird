5a635efa6fbe671d6b18b1f495f88d71d45a8453
x421 <x421@server.fake>
2013-11-07 14:23:44 +0100

    New Icons

--- assets/render-graphics.sh
+++ assets/render-graphics.sh
@@ -5,13 +5,16 @@ sizes=(16 22 24 32 48 256 )
 
 for size in ${sizes[@]}
 do
-  mkdir -p ${size}
+  mkdir -p ${size}x${size}
   rsvg-convert ./corebird.svg --width="${size}" --height="${size}" \
                --format=png -o "./${size}x${size}/corebird_${size}.png"
 done
 
-svg-convert ./no_avatar.svg --width="22" --height="22" \
+rsvg-convert ./no_avatar.svg --width="22" --height="22" \
                --format=png -o "./no_avatar.png"
               
-svg-convert ./no_banner.svg --width="360" --height="180" \
+rsvg-convert ./no_banner.svg --width="360" --height="180" \
                --format=png -o "./no_banner.png"
+               
+rsvg-convert ./verified.svg --width="360" --height="180" \
+               --format=png -o "./verified.png"
