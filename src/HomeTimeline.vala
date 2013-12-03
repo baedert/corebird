baedert/corebird

commit 45ab3afd52893e521ced2839cd42e4a485c22959

HomeTimeline: Don't force_wait on scroll_top_next
This caused the user to be unable to scroll down whenever he received
mutliple tweets when not being at the HomeTimeline when they arrived.
Related to #49.

src/HomeTimeline.vala
@@ -37,7 +37,7 @@ class HomeTimeline : IMessageReceiver, DefaultTimeline {
 
       this.balance_next_upper_change (TOP);
       if (this.scrolled_up && (t.user_id == account.id || auto_scroll)) {
-        this.scroll_up_next (true, true);
+        this.scroll_up_next (true);
       }
 
       var entry = new TweetListEntry(t, main_window, account);

commit 48644e37635790fdf3c15650ceab2ce533cd1fda

HomeTimeline: Don't forget to scroll up if necessary
Broken by 45ab3afd52893e521ced2839cd42e4a485c22959

src/HomeTimeline.vala
@@ -37,7 +37,8 @@ class HomeTimeline : IMessageReceiver, DefaultTimeline {
 
       this.balance_next_upper_change (TOP);
       if (this.scrolled_up && (t.user_id == account.id || auto_scroll)) {
-        this.scroll_up_next (true);
+        this.scroll_up_next (true, false,
+                             main_window.cur_page_id != this.id);
       }
 
       var entry = new TweetListEntry(t, main_window, account);