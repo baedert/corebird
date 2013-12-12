baedert/corebird

commit 1bb2d37e6f5c8521048c53ce43c75903af308c3c

ProfilePage: Show if the shown user follows the authenticating user

src/ProfilePage.vala
@@ -54,6 +54,8 @@ class ProfilePage : ScrollWidget, IPage {
   [GtkChild]
   private Gtk.Spinner progress_spinner;
   [GtkChild]
+  private Gtk.Label follows_you_label;
+  [GtkChild]
   private Gtk.MenuItem dm_menu_item;
   [GtkChild]
   private Gtk.MenuItem tweet_to_menu_item;
@@ -100,7 +102,7 @@ class ProfilePage : ScrollWidget, IPage {
     follow_button.sensitive = (user_id != account.id);
 
     load_banner (DATADIR + "/no_banner.png");
-    //load_friendship();
+    load_friendship();
     //Load cached data
     Corebird.db.select ("profiles").cols ("id", "screen_name", "name", "description", "tweets",
      "following", "followers", "avatar_name", "banner_url", "url", "location", "is_following",
@@ -135,6 +137,33 @@ class ProfilePage : ScrollWidget, IPage {
     });
   } // }}}
 
+
+  private void load_friendship () {
+    var call = account.proxy.new_call ();
+    call.set_function ("1.1/friendships/show.json");
+    call.set_method ("GET");
+    call.add_param ("source_id", account.id.to_string ());
+    call.add_param ("target_id", user_id.to_string ());
+    call.invoke_async.begin (null, (o, res) => {
+      try {
+        call.invoke_async.end (res);
+      } catch (GLib.Error e) {
+        Utils.show_error_object (call.get_payload (), e.message);
+        return;
+      }
+      var parser = new Json.Parser ();
+      stdout.printf (call.get_payload() + "\n");
+      try {
+        parser.load_from_data (call.get_payload ());
+      } catch (GLib.Error e) {
+        critical ("%s:\n%s", e.message, call.get_payload ());
+        return;
+      }
+      bool followed_by = parser.get_root ().get_object ().get_object_member ("relationship")
+                               .get_object_member ("target").get_boolean_member ("following");
+      follows_you_label.visible = followed_by;
+    });
+  }
 
   private async void load_profile_data (int64 user_id) { //{{{
     progress_spinner.show ();

ui/profile-page.ui
@@ -246,6 +246,24 @@
                         <property name="position">1</property>
                       </packing>
                     </child>
+                    <child>
+                      <object class="GtkLabel" id="follows_you_label">
+                        <property name="can_focus">False</property>
+                        <property name="no_show_all">True</property>
+                        <property name="halign">end</property>
+                        <property name="margin_right">3</property>
+                        <property name="margin_top">4</property>
+                        <property name="label" translatable="yes">Follows you</property>
+                        <style>
+                          <class name="dim-label"/>
+                        </style>
+                      </object>
+                      <packing>
+                        <property name="expand">False</property>
+                        <property name="fill">True</property>
+                        <property name="position">2</property>
+                      </packing>
+                    </child>
                   </object>
                   <packing>
                     <property name="expand">False</property>

commit 8956db2aa2998b4a2d0169629d339dd0e06dcba8

ProfilePage: Remove debugging output
Fallout from 1bb2d37e6f5c8521048c53ce43c75903af308c3c

src/ProfilePage.vala
@@ -152,7 +152,6 @@ class ProfilePage : ScrollWidget, IPage {
         return;
       }
       var parser = new Json.Parser ();
-      stdout.printf (call.get_payload() + "\n");
       try {
         parser.load_from_data (call.get_payload ());
       } catch (GLib.Error e) {