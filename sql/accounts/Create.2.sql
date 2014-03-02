696f91a9e97282220be41cd12608a0b00dea3ba2
Timm Bäder <mail@baedert.org>
2014-03-02 18:08:33 +0100

    sql/accounts/Create.2.sql: Add default value for block_count

--- sql/accounts/Create.2.sql
+++ sql/accounts/Create.2.sql
@@ -5,5 +5,5 @@ PRAGMA user_version = 2;
 CREATE TABLE IF NOT EXISTS `filters`(
   id INTEGER(3) PRIMARY KEY,
   content VARCHAR(100),
-  block_count INTEGER(5)
+  block_count INTEGER(5) DEFAULT 0
 );
