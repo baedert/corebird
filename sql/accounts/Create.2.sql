bb1c31e48163e3d7427274f7243a195af52bb1a3
Timm Bäder <mail@baedert.org>
2014-03-09 19:06:34 +0100

    Remove block_count from the filter sql schema

--- sql/accounts/Create.2.sql
+++ sql/accounts/Create.2.sql
@@ -4,6 +4,5 @@ PRAGMA user_version = 2;
 
 CREATE TABLE IF NOT EXISTS `filters`(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  content VARCHAR(100),
-  block_count INTEGER(5) DEFAULT 0
+  content VARCHAR(100)
 );
