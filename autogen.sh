429739f2361878c038bbbbb9b357c8509aba9942
Timm Bäder <mail@baedert.org>
2014-04-18 09:40:00 +0200

    autogen.sh: touch config.rpath

--- autogen.sh
+++ autogen.sh
@@ -6,18 +6,9 @@ test -z "$srcdir" && srcdir=.
 ORIGDIR=`pwd`
 cd $srcdir
 
-test -z "$VALAC" && VALAC=valac
-if ! $VALAC --version | sed -e 's/^Vala \([0-9]\+\.[0-9]\+\).*$/\1/' | grep -vq '^0\.\([0-9]\|1[0-1]\)$'
-then
-    echo "**Error**: You must have valac >= 0.12.0 installed"
-    echo "  to build vala. Download the appropriate package"
-    echo "  from your distribution or get the source tarball at"
-    echo "  http://download.gnome.org/sources/vala/"
-    exit 1
-fi
-
 # Automake requires that ChangeLog exist.
 touch ChangeLog
+touch config.rpath
 mkdir -p m4
 
 rm -f .version
