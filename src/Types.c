926600aa290fcde9309c30818a20eb76c4113cba
Timm Bäder <mail@baedert.org>
2016-11-06 16:36:34 +0100

    MiniTweet: Remove duplicate thumb_urls/urls
    Some tweets have videos attached with a thumb_url that matches the url
    of another attached image.

--- src/Types.c
+++ src/Types.c
@@ -494,6 +494,7 @@ cb_mini_tweet_parse_entities (CbMiniTweet *t,
               if (is_media_candidate (url))
                 {
                   t->medias[t->n_medias] = cb_media_new ();
+                  t->medias[t->n_medias]->type = CB_MEDIA_TYPE_IMAGE;
                   t->medias[t->n_medias]->url = g_strdup (url);
                   t->medias[t->n_medias]->target_url = g_strdup_printf ("%s:orig", url);
 
@@ -544,13 +545,32 @@ cb_mini_tweet_parse_entities (CbMiniTweet *t,
 
               if (variant != NULL)
                 {
-                  t->medias[t->n_medias] = cb_media_new ();
-                  t->medias[t->n_medias]->url = g_strdup (json_object_get_string_member (variant, "url"));
-                  t->medias[t->n_medias]->thumb_url = g_strdup (json_object_get_string_member (media_obj, "media_url"));
-                  t->medias[t->n_medias]->type   = CB_MEDIA_TYPE_TWITTER_VIDEO;
-                  t->medias[t->n_medias]->width  = thumb_width;
-                  t->medias[t->n_medias]->height = thumb_height;
-                  t->n_medias ++;
+                  int n_media = t->n_medias;
+                  const char *thumb_url = json_object_get_string_member (media_obj, "media_url");
+                  /* Some tweets have both a video and a thumbnail for that video attached. The tweet json
+                   * will list the image first. The url of the image and the thumb_url of the video will match
+                   */
+                  for (k = 0; k < t->n_medias; k ++)
+                    {
+                      if (t->medias[k]->type == CB_MEDIA_TYPE_IMAGE &&
+                          strcmp (t->medias[k]->url, thumb_url) == 0)
+                        {
+                          /* Replace this media */
+                          g_object_unref (t->medias[k]);
+                          n_media = k;
+                          break;
+                        }
+                    }
+
+                  t->medias[n_media] = cb_media_new ();
+                  t->medias[n_media]->url = g_strdup (json_object_get_string_member (variant, "url"));
+                  t->medias[n_media]->thumb_url = g_strdup (thumb_url);
+                  t->medias[n_media]->type   = CB_MEDIA_TYPE_TWITTER_VIDEO;
+                  t->medias[n_media]->width  = thumb_width;
+                  t->medias[n_media]->height = thumb_height;
+
+                  if (n_media == t->n_medias)
+                    t->n_medias ++;
                 }
             }
           else
