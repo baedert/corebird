78ea7f5e7a303de40961070764bc28b818bf94f2
Timm Bäder <mail@baedert.org>
2016-12-21 10:09:50 +0100

    Move icons into hicolor/

--- data/render-icons.sh
+++ data/render-icons.sh
@@ -7,5 +7,5 @@ sizes=(16 20 24 32 36 40 48 64 96 128 192 512)
 for size in ${sizes[@]}
 do
   rsvg-convert ./corebird.svg --width="${size}" --height="${size}" \
-               --format=png -o "./${size}x${size}/corebird.png"
+               --format=png -o "./hicolor/${size}x${size}/corebird.png"
 done
