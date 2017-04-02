656741a8d36ad08439f997ad4d6962b168c7e63b
Timm Bäder <mail@baedert.org>
2017-04-02 14:43:43 +0200

    avatarcache: Add license headers

--- src/CbAvatarCache.c
+++ src/CbAvatarCache.c
@@ -1,3 +1,20 @@
+/*  This file is part of corebird, a Gtk+ linux Twitter client.
+ *  Copyright (C) 2016 Timm Bäder
+ *
+ *  corebird is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  corebird is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with corebird.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "CbAvatarCache.h"
 
 
@@ -16,6 +33,7 @@ static void
 cache_entry_destroy (CacheEntry *entry)
 {
   g_free (entry->url);
+
   if (entry->surface)
     cairo_surface_destroy (entry->surface);
 }
@@ -64,7 +82,7 @@ cb_avatar_cache_add (CbAvatarCache   *cache,
       if (surface)
         entry->surface = cairo_surface_reference (surface);
       entry->url = g_strdup (url);
-      /* The subsequence incrase_refcount will push this up to 1. Entries with
+      /* The subsequent incrase_refcount will push this up to 1. Entries with
        * refcount 0 won't be removed from the cache until a decrease_refcount call */
       entry->refcount = 0;
     }
@@ -148,6 +166,7 @@ cb_avatar_cache_set_url (CbAvatarCache *cache,
 
   if (entry->url != NULL)
     g_free (entry->url);
+
   entry->url = g_strdup (url);
 }
 
@@ -182,7 +201,7 @@ cb_avatar_cache_decrease_refcount_for_surface (CbAvatarCache   *cache,
 
   entry->refcount --;
 
-  if (entry->refcount == 0)
+  if (entry->refcount <= 0)
     {
       g_debug ("Removing avatar with id %ld from cache", entry->user_id);
       g_array_remove_index_fast (cache->entries, index);
