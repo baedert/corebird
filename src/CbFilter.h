41d84581fe8ab2efdaf637a5ed75f7c58a5a2cd5
Timm Bäder <mail@baedert.org>
2017-08-19 10:53:21 +0200

    filter: Whitespace

--- src/CbFilter.h
+++ src/CbFilter.h
@@ -36,19 +36,13 @@ struct _CbFilter
 G_DECLARE_FINAL_TYPE (CbFilter, cb_filter, CB, FILTER, GObject);
 
 
-GType cb_filter_get_type (void) G_GNUC_CONST;
-
-CbFilter *cb_filter_new (const char *expr);
-
-void  cb_filter_reset (CbFilter *filter, const char *expr);
-
-gboolean cb_filter_matches (CbFilter *filter, const char *text);
-
+GType       cb_filter_get_type     (void) G_GNUC_CONST;
+CbFilter   *cb_filter_new          (const char *expr);
+void        cb_filter_reset        (CbFilter *filter, const char *expr);
+gboolean    cb_filter_matches      (CbFilter *filter, const char *text);
 const char *cb_filter_get_contents (CbFilter *filter);
-
-int cb_filter_get_id (CbFilter *filter);
-
-void cb_filter_set_id (CbFilter *filter, int id);
+int         cb_filter_get_id       (CbFilter *filter);
+void        cb_filter_set_id       (CbFilter *filter, int id);
 
 G_END_DECLS
 
