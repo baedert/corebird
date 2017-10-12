b01755b369354ca60cbcea49a95f40893fba75fa
Timm Bäder <mail@baedert.org>
2017-10-12 09:25:45 +0200

    Reimplement tweet rows

---
+++ src/CbTweetRow.c
@@ -0,0 +1,249 @@
+/*  This file is part of corebird, a Gtk+ linux Twitter client.
+ *  Copyright (C) 2017 Timm Bäder
+ *
+ *  corebird is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  corebird is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with corebird.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "CbTweetRow.h"
+#include "CbUtils.h"
+#include "corebird.h"
+
+
+G_DEFINE_TYPE (CbTweetRow, cb_tweet_row, GTK_TYPE_LIST_BOX_ROW);
+
+
+static void
+cb_tweet_row_measure (GtkWidget      *widget,
+                      GtkOrientation  orientation,
+                      int             for_size,
+                      int            *minimum,
+                      int            *natural,
+                      int            *minimum_baseline,
+                      int            *natural_baseline)
+{
+
+  if (orientation == GTK_ORIENTATION_HORIZONTAL)
+    {
+
+    }
+  else /* VERTICAL */
+    {
+      *minimum = 100;
+      *natural = 100;
+    }
+}
+
+static void
+cb_tweet_row_size_allocate (GtkWidget           *widget,
+                            const GtkAllocation *allocation,
+                            int                  baseline,
+                            GtkAllocation       *out_clip)
+{
+  CbTweetRow *self = (CbTweetRow *)widget;
+  GtkAllocation child_alloc;
+  int min_width, nat_width;
+  int min_height, nat_height;
+  int avatar_width;
+  int top_row_height;
+  GtkAllocation child_clip;
+
+  gtk_widget_measure (self->avatar_widget, GTK_ORIENTATION_HORIZONTAL, -1, &min_width, &nat_width, NULL, NULL);
+  gtk_widget_measure (self->avatar_widget, GTK_ORIENTATION_VERTICAL, -1, &min_height, &nat_height, NULL, NULL);
+  child_alloc.x = 0;
+  child_alloc.y = 0;
+  child_alloc.width = min_width;
+  child_alloc.height = min_height;
+  gtk_widget_size_allocate (self->avatar_widget, &child_alloc, -1, &child_clip);
+  avatar_width = min_width;
+
+  gtk_widget_measure (self->top_row_box, GTK_ORIENTATION_HORIZONTAL, -1, &min_width, &nat_width, NULL, NULL);
+  gtk_widget_measure (self->top_row_box, GTK_ORIENTATION_VERTICAL, -1, &min_height, &nat_height, NULL, NULL);
+  child_alloc.x += child_alloc.width;
+  child_alloc.width = MAX (allocation->width - child_alloc.width, min_width);
+  child_alloc.height = min_height;
+  gtk_widget_size_allocate (self->top_row_box, &child_alloc, -1, &child_clip);
+  top_row_height = min_height;
+
+  if (self->reply_label != NULL)
+    {
+      gtk_widget_measure (self->reply_label, GTK_ORIENTATION_HORIZONTAL, -1, &min_width, &nat_width,
+                          NULL, NULL);
+      child_alloc.width = MAX (min_width, allocation->width - avatar_width);
+      gtk_widget_measure (self->reply_label, GTK_ORIENTATION_VERTICAL, child_alloc.width,
+                          &min_height, &nat_height, NULL, NULL);
+      child_alloc.height = nat_height;
+      child_alloc.y += top_row_height;
+      gtk_widget_size_allocate (self->reply_label, &child_alloc, -1, &child_clip);
+    }
+
+  child_alloc.y += nat_height;
+  gtk_widget_measure (self->text_label, GTK_ORIENTATION_HORIZONTAL, -1, &min_width, &nat_width, NULL, NULL);
+  child_alloc.width = MAX (allocation->width - avatar_width, min_width);
+  gtk_widget_measure (self->text_label, GTK_ORIENTATION_VERTICAL, child_alloc.width,
+                      &min_height, &nat_height, NULL, NULL);
+  child_alloc.x = avatar_width;
+  child_alloc.height = nat_height;
+  gtk_widget_size_allocate (self->text_label, &child_alloc, -1, &child_clip);
+
+  if (self->rt_image != NULL)
+    {
+      g_assert (self->rt_label != NULL);
+
+      gtk_widget_measure (self->rt_image, GTK_ORIENTATION_HORIZONTAL, -1, &min_width, &nat_width,
+                          NULL, NULL);
+      gtk_widget_measure (self->rt_image, GTK_ORIENTATION_VERTICAL, -1, &min_height, &nat_height,
+                          NULL, NULL);
+      child_alloc.x = avatar_width - min_width;
+      child_alloc.y = child_alloc.y + child_alloc.height;
+      child_alloc.width = min_width;
+      child_alloc.height = min_height;
+      gtk_widget_size_allocate (self->rt_image, &child_alloc, -1, &child_clip);
+
+      gtk_widget_measure (self->rt_label, GTK_ORIENTATION_HORIZONTAL, -1, &min_width, &nat_width,
+                          NULL, NULL);
+      gtk_widget_measure (self->rt_label, GTK_ORIENTATION_VERTICAL, -1, &min_height, &nat_height,
+                          NULL, NULL);
+      child_alloc.x += child_alloc.width;
+      child_alloc.width = MAX (allocation->width - avatar_width, min_width);
+      child_alloc.height = min_height; // TODO: Should be centered (or "baseline" aligned) wrt the rt_image
+      gtk_widget_size_allocate (self->rt_label, &child_alloc, -1, &child_clip);
+    }
+
+  if (self->mm_widget != NULL)
+    {
+      child_alloc.width = MAX (allocation->width - avatar_width, min_width);
+      gtk_widget_measure (self->mm_widget, GTK_ORIENTATION_HORIZONTAL, -1, &min_width, &nat_width,
+                          NULL, NULL);
+      gtk_widget_measure (self->mm_widget, GTK_ORIENTATION_VERTICAL, child_alloc.width,
+                          &min_height, &nat_height, NULL, NULL);
+      child_alloc.x = avatar_width - min_width;
+      child_alloc.y = child_alloc.y + child_alloc.height;
+      child_alloc.height = min_height;
+      gtk_widget_size_allocate (self->mm_widget, &child_alloc, -1, &child_clip);
+    }
+}
+
+static void
+cb_tweet_row_class_init (CbTweetRowClass *klass)
+{
+  GtkWidgetClass *widget_class = (GtkWidgetClass *)klass;
+
+  widget_class->measure = cb_tweet_row_measure;
+  widget_class->size_allocate = cb_tweet_row_size_allocate;
+}
+
+static void
+cb_tweet_row_init (CbTweetRow *self)
+{
+}
+
+static void
+create_ui (CbTweetRow *self)
+{
+  g_assert (self->tweet != NULL);
+
+  self->avatar_widget = (GtkWidget *)avatar_widget_new ();
+  gtk_widget_set_parent (self->avatar_widget, (GtkWidget *)self);
+  self->top_row_box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+  gtk_widget_set_parent (self->top_row_box, (GtkWidget *)self);
+
+  twitter_get_avatar (twitter_get (), cb_tweet_get_user_id (self->tweet), self->tweet->avatar_url,
+                      (AvatarWidget *)self->avatar_widget, 48, FALSE, NULL, NULL);
+
+  self->name_button = (GtkWidget *)text_button_new ();
+  text_button_set_markup ((TextButton*)self->name_button, cb_tweet_get_user_name (self->tweet));
+  gtk_container_add (GTK_CONTAINER (self->top_row_box), self->name_button);
+
+  self->screen_name_label = gtk_label_new (g_strdup_printf ("@%s", cb_tweet_get_screen_name (self->tweet)));
+  gtk_style_context_add_class (gtk_widget_get_style_context (self->screen_name_label),
+                               "dim-label");
+  gtk_widget_set_hexpand (self->screen_name_label, TRUE);
+  gtk_widget_set_halign (self->screen_name_label, GTK_ALIGN_START);
+  gtk_container_add (GTK_CONTAINER (self->top_row_box), self->screen_name_label);
+
+  self->time_delta_label = gtk_label_new ("Foo");
+  gtk_container_add (GTK_CONTAINER (self->top_row_box), self->time_delta_label);
+
+  /* Reply label */
+  if (self->tweet->source_tweet.reply_id != 0 ||
+      (self->tweet->retweeted_tweet != NULL && self->tweet->retweeted_tweet->reply_id != 0))
+    {
+      GString *str = g_string_new (NULL);
+
+      if (self->tweet->retweeted_tweet != NULL)
+        cb_utils_write_reply_text (self->tweet->retweeted_tweet, str);
+      else
+        cb_utils_write_reply_text (&self->tweet->source_tweet, str);
+
+      self->reply_label = gtk_label_new (g_string_free (str, FALSE)); // Still leaks
+      gtk_label_set_xalign (GTK_LABEL (self->reply_label), 0.0f);
+      gtk_label_set_yalign (GTK_LABEL (self->reply_label), 0.0f);
+      gtk_label_set_use_markup (GTK_LABEL (self->reply_label), TRUE);
+      gtk_label_set_track_visited_links (GTK_LABEL (self->reply_label), FALSE);
+      gtk_label_set_line_wrap (GTK_LABEL (self->reply_label), TRUE);
+      gtk_label_set_line_wrap_mode (GTK_LABEL (self->reply_label), PANGO_WRAP_WORD_CHAR);
+      gtk_style_context_add_class (gtk_widget_get_style_context (self->reply_label),
+                                   "dim-label");
+      gtk_style_context_add_class (gtk_widget_get_style_context (self->reply_label),
+                                   "invisible-links");
+      gtk_widget_set_parent (self->reply_label, (GtkWidget *)self);
+    }
+
+  self->text_label = gtk_label_new (cb_tweet_get_trimmed_text (self->tweet, 0));
+  gtk_label_set_xalign (GTK_LABEL (self->text_label), 0.0f);
+  gtk_label_set_yalign (GTK_LABEL (self->text_label), 0.0f);
+  gtk_label_set_use_markup (GTK_LABEL (self->text_label), TRUE);
+  gtk_label_set_track_visited_links (GTK_LABEL (self->text_label), FALSE);
+  gtk_label_set_line_wrap (GTK_LABEL (self->text_label), TRUE);
+  gtk_label_set_line_wrap_mode (GTK_LABEL (self->text_label), PANGO_WRAP_WORD_CHAR);
+  gtk_widget_set_parent (self->text_label, (GtkWidget *)self);
+
+  /* Retweet indicators */
+  if (self->tweet->retweeted_tweet != NULL)
+    {
+      self->rt_image = gtk_image_new_from_icon_name ("corebird-retweet-symbolic",
+                                                     GTK_ICON_SIZE_MENU);
+      gtk_widget_set_parent (self->rt_image, (GtkWidget *)self);
+
+      self->rt_label = gtk_label_new (self->tweet->source_tweet.author.user_name);
+      gtk_widget_set_halign (self->rt_label, GTK_ALIGN_START);
+      gtk_widget_set_parent (self->rt_label, (GtkWidget *)self);
+    }
+
+  /* Inline media */
+  if (cb_tweet_has_inline_media (self->tweet))
+    {
+      int n_medias;
+      CbMedia ** medias = cb_tweet_get_medias (self->tweet, &n_medias);
+      self->mm_widget = (GtkWidget *)multi_media_widget_new ();
+      gtk_widget_set_parent (self->mm_widget, (GtkWidget *)self);
+
+      multi_media_widget_set_all_media ((MultiMediaWidget *)self->mm_widget, medias, n_medias);
+
+      // TODO: Care about NSFW-ness
+    }
+
+  gtk_style_context_add_class (gtk_widget_get_style_context ((GtkWidget *)self), "tweet");
+}
+
+GtkWidget *
+cb_tweet_row_new (CbTweet *tweet)
+{
+  CbTweetRow *self  = (CbTweetRow *)g_object_new (CB_TYPE_TWEET_ROW, NULL);
+
+  g_set_object (&self->tweet, tweet);
+  create_ui (self);
+
+  return (GtkWidget *)self;
+}
---
+++ src/CbTweetRow.h
@@ -0,0 +1,54 @@
+/*  This file is part of corebird, a Gtk+ linux Twitter client.
+ *  Copyright (C) 2017 Timm Bäder
+ *
+ *  corebird is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  corebird is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with corebird.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __CB_TWEET_ROW_H__
+#define __CB_TWEET_ROW_H__
+
+#include <gtk/gtk.h>
+#include "CbTweet.h"
+
+struct _CbTweetRow
+{
+  GtkListBoxRow parent_instance;
+
+  CbTweet *tweet;
+
+  GtkWidget *stack;
+  GtkWidget *avatar_widget;
+  GtkWidget *name_button;
+  GtkWidget *screen_name_label;
+  GtkWidget *time_delta_label;
+  GtkWidget *text_label;
+  GtkWidget *top_row_box;
+
+  /* Only conditionally created widgets */
+  GtkWidget *rt_image;
+  GtkWidget *rt_label;
+  GtkWidget *reply_label;
+  GtkWidget *mm_widget;
+};
+typedef struct _CbTweetRow CbTweetRow;
+
+
+#define CB_TYPE_TWEET_ROW cb_tweet_row_get_type ()
+G_DECLARE_FINAL_TYPE (CbTweetRow, cb_tweet_row, CB, TWEET_ROW, GtkListBoxRow);
+
+
+GtkWidget *  cb_tweet_row_new (CbTweet *tweet);
+
+
+#endif
