77b6dc203498ceedaa155ac6c2d219beaa62d6e5
Timm Bäder <mail@baedert.org>
2017-10-29 11:46:44 +0100

    CbUserCounter: Reindent

--- src/CbUserCounter.h
+++ src/CbUserCounter.h
@@ -50,26 +50,21 @@ struct _CbUserCounter
 G_DECLARE_FINAL_TYPE (CbUserCounter, cb_user_counter, CB, USER_COUNTER, GObject);
 
 
-GType cb_user_counter_get_type (void) G_GNUC_CONST;
-
-CbUserCounter * cb_user_counter_new (void);
-
-void  cb_user_counter_id_seen (CbUserCounter        *counter,
-                               const CbUserIdentity *id);
-
-void  cb_user_counter_user_seen (CbUserCounter *counter,
-                                 gint64         user_id,
-                                 const char    *screen_name,
-                                 const char    *user_name);
-
-int  cb_user_counter_save (CbUserCounter *counter, sqlite3 *db);
-
-void cb_user_counter_query_by_prefix (CbUserCounter *counter,
-                                      sqlite3       *db,
-                                      const char    *prefix,
-                                      int            max_results,
-                                      CbUserInfo   **results,
-                                      int           *n_results);
+CbUserCounter * cb_user_counter_new             (void);
+void            cb_user_counter_id_seen         (CbUserCounter        *counter,
+                                                 const CbUserIdentity *id);
+void            cb_user_counter_user_seen       (CbUserCounter *counter,
+                                                 gint64         user_id,
+                                                 const char    *screen_name,
+                                                 const char    *user_name);
+int             cb_user_counter_save            (CbUserCounter *counter,
+                                                 sqlite3       *db);
+void            cb_user_counter_query_by_prefix (CbUserCounter *counter,
+                                                 sqlite3       *db,
+                                                 const char    *prefix,
+                                                 int            max_results,
+                                                 CbUserInfo   **results,
+                                                 int           *n_results);
 
 /* CbUserInfo */
 void cb_user_info_destroy (CbUserInfo *info);
