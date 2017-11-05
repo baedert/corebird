b4ef882d40df3cbde4bcdde5be49cfe3a0e16980
Timm Bäder <mail@baedert.org>
2017-11-05 07:05:51 +0100

    Add CbQuoteTweetWidget

---
+++ src/CbQuoteTweetWidget.h
@@ -0,0 +1,36 @@
+/*  This file is part of corebird, a Gtk+ linux Twitter client.
+ *  Copyright (C) 2017 Timm Bäder
+ *
+ *  corebird is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  corebird is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with corebird.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __CB_QUOTE_TWEET_WIDGET_H__
+#define __CB_QUOTE_TWEET_WIDGET_H__
+
+#include <gtk/gtk.h>
+#include "CbTypes.h"
+
+struct _CbQuoteTweetWidget
+{
+  GtkWidget parent_instance;
+
+};
+typedef struct _CbQuoteTweetWidget CbQuoteTweetWidget;
+
+#define CB_TYPE_QUOTE_TWEET_WIDGET cb_quote_tweet_widget_get_type ()
+G_DECLARE_FINAL_TYPE (CbQuoteTweetWidget, cb_quote_tweet_widget, CB, QUOTE_TWEET_WIDGET, GtkWidget);
+
+GtkWidget * cb_quote_tweet_widget_new (const CbMiniTweet *quote);
+
+#endif
