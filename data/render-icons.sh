1d6bd2d46fc3870e71ee73b882e0ea8a5a0486c5
Timm Bäder <mail@baedert.org>
2018-04-08 15:28:38 +0200

    add zopflipng compression to render-icons script
    See #826

--- data/render-icons.sh
+++ data/render-icons.sh
@@ -2,10 +2,16 @@
 
 # Oh god I hate bash.
 
-sizes=(16  24 32 48 64 96 128 256)
+sizes=(16 24 32 48 64 96 128 256)
 
 for size in ${sizes[@]}
 do
+  name="./hicolor/${size}x${size}/corebird.png"
+
   rsvg-convert ./corebird.svg --width="${size}" --height="${size}" \
-               --format=png -o "./hicolor/${size}x${size}/corebird.png"
+               --format=png -o ${name}
+  # https://github.com/baedert/corebird/pull/826
+
+  zopflipng --iterations=500 --keepchunks=iCCP \
+    --lossy_transparent --splitting=3 -my ${name} ${name}
 done
